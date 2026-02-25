180c4e2e3c81e56fbdf61bd05779b4f86ae22238
Sushmoy <sushmoy.r@utkorsho.org>
2026-02-25 17:56:44 +0600

    chore: configure tsconfig, next-intl plugin, shadcn/ui, and env

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from "next";
+import createNextIntlPlugin from "next-intl/plugin";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
-};
+const nextConfig: NextConfig = {};
 
-export default nextConfig;
+const withNextIntl = createNextIntlPlugin("./src/i18n/config.ts");
+export default withNextIntl(nextConfig);
